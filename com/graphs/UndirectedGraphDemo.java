a364f378789bed6766775554bfb00ccdcbfe0b97
iqbalzaveri <iqbalzaveri@gmail.com>
2020-09-11 19:06:55 -0500

    Update UndirectedGraphDemo.java

--- com/graphs/UndirectedGraphDemo.java
+++ com/graphs/UndirectedGraphDemo.java
@@ -5,6 +5,14 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 
+class QueueNode {
+    int vertex;
+    int dist; //distance from src node  to the  current node(vertex)
+    QueueNode(int vertex, int dist) {
+        this.vertex = vertex;
+        this.dist =  dist;
+    }
+}
 class UndirectedGraph {
     LinkedList<Integer>[] adjList; //adjacency list
 
@@ -59,6 +67,48 @@ class UndirectedGraph {
       } //for loop
     }
 
+    
+    int shortestPathCountUsingBFS(int src, int dest) {
+      boolean[] isVisited = new boolean[adjList.length];
+      Deque<QueueNode> queue = new ArrayDeque<>();
+
+       queue.offer(new QueueNode(src,0));
+       isVisited[src] = true; //mark it as visited
+
+
+        while (!queue.isEmpty()) {
+          
+          QueueNode qNode = queue.poll();
+          //process node
+          Integer v = qNode.vertex;
+          int dist = qNode.dist;
+          if(v == dest) { //destination found!
+              return dist;
+          }
+
+
+          //get all neighbors
+          LinkedList<Integer> list = adjList[v];
+          if(list != null) {
+            for(Integer c : list) {
+              if(c != null && !isVisited[c]) {
+                queue.offer(new QueueNode(c, dist+1);
+                isVisited[c] = true; //mark it as visited
+              }
+            }
+          }
+
+        } // enf of while loop
+        return -1;
+    }
+                            
+ 
+                            
+                            
+    
+    
+    
+    
   public void printGraphUsingDFS() {
     boolean[] isVisited = new boolean[adjList.length];
     for(int i=0; i<adjList.length; i++) {
